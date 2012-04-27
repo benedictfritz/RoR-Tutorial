a52453b4d814cefd44daafa5bd682b96d71183ad
Benedict Fritz <benedictfritz@gmail.com>
2012-04-27 14:00:49 -0500

    sloppily split stuff into chapter folders.

---
+++ chapter1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ae5fa0592b1997846fada17eb1d882d2b818f21c3646e2bb961a57f951fc60fea430eed4699294d755425ef53d412f69d71c66333e3b85c03161cf54afef2d98'
