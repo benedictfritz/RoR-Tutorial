c88f6329c477745a1e742dcbeb159fc3e53bf2f4
Benedict Fritz <benedictfritz@gmail.com>
2012-04-27 14:25:44 -0500

    initial commit of chapter2 stuff.

---
+++ chapter2/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '24e786b69e5e946814d4a838d7c86af3913538f5b25c1bb074d6ac813af3424c16b7c7051a0817a4f2cd73a71caeaa1e5821295eca4c827737b99c60995c9e04'
